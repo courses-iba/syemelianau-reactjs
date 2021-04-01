dbdaf80ed69a240ac210c57e6f40a34589d00d65
SYemelianau <SYemelianau@gomel.iba.by>
2021-04-01 23:33:14 +0300

    reactjs - task2 - Card active state added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
+
 import styles from './Card.module.css';
 import Divider from './Divider';
 
-const Card = ({ title, children }) => (
-    <div className={styles.card}>
-        <div className={styles.header}>{title}</div>
-        <Divider />
-        <div className={styles.content}>{children}</div>
-    </div>
-);
+const Card = ({ title, children }) => {
+    const [color, setColor] = useState(undefined);
+
+    const randomColor = () => Math.floor(Math.random() * 5) + 1;
+    const toggleColor = () => setColor(color ? undefined : randomColor());
+
+    return (
+        <div className={[styles.card, styles[`card${color}`]].join(' ')} onClick={toggleColor}>
+            <div className={styles.header}>{title}</div>
+            <Divider />
+            <div className={styles.content}>{children}</div>
+        </div>
+    );
+};
 
 export default Card;
