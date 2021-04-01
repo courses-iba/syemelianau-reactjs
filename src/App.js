646ae2a1ae0b5d84a8c247e13a7b9510aa2372dd
SYemelianau <SYemelianau@gomel.iba.by>
2021-04-01 17:18:17 +0300

    reactjs - task1 - Separated variables & card numbers

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Card from './components/Card';
 
-const App = () => (
-    <div className={styles.app}>
-        <Header title="React App" />
-        <Content>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map(value => (
-                <Card title="Card Title" key={value}>
-                    Some quick example text
-                    to build on the card title
-                    and make up the bulk of the card's content.
-                </Card>
-            ))}
-        </Content>
-    </div>
-);
+const App = () => {
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+    const cards = data.map((value, index) => (
+        <Card title={`Card Title ${value}`} key={index}>
+            {value}.
+            Some quick example text
+            to build on the card title
+            and make up the bulk of the card's content.
+        </Card>
+    ));
+
+    return (
+        <div className={styles.app}>
+            <Header title="React App" />
+            <Content>{cards}</Content>
+        </div>
+    );
+};
 
 export default App;
