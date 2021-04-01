96f49fef3ca45de64a7b15122747346f84195159
SYemelianau <SYemelianau@gomel.iba.by>
2021-04-01 18:50:25 +0300

    reactjs - task1 - Implemented card data interface

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import Content from './components/Content';
 import Card from './components/Card';
 
 const App = () => {
-    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-    const cards = data.map((value, index) => (
-        <Card title={`Card Title ${value}`} key={index}>
-            {value}.
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+    const data = array.map((value, index) => ({
+        id: index,
+        title: `Card Title ${value}`,
+        content: `
+            ${value}.
             Some quick example text
             to build on the card title
             and make up the bulk of the card's content.
-        </Card>
+        `
+    }));
+
+    const cards = data.map(({ id, title, content }) => (
+        <Card
+            key={id}
+            title={title}
+            children={content}
+        />
     ));
 
     return (
