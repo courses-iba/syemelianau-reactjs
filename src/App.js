9a93571725d1b066b4e5221b211809dc99f960cd
SYemelianau <SYemelianau@gomel.iba.by>
2021-04-01 16:11:29 +0300

    reactjs - task1 - Key prop added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => (
     <div className={styles.app}>
         <Header title="React App" />
         <Content>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => (
-                <Card title="Card Title">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map(value => (
+                <Card title="Card Title" key={value}>
                     Some quick example text
                     to build on the card title
                     and make up the bulk of the card's content.
