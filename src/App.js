91d34879a1d1fd9c4897ef117a7b3da4ff471663
SYemelianau <SYemelianau@gomel.iba.by>
2021-04-03 23:08:18 +0300

    reactjs - task7 - Added cards creation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { useState } from 'react';
-import { MdDelete } from 'react-icons/md';
+import { v4 as uuid } from 'uuid';
+import { MdDelete, MdAdd } from 'react-icons/md';
 
 import styles from './App.module.css';
 import { iButton } from './styles/Button.module.css';
 import Header from './containers/Header';
 import Content from './containers/Content';
-import CardList from './components/CardList';
 import Menu from './containers/Menu';
 import Checkbox from './components/Checkbox';
+import CardList from './components/CardList';
 
 const App = () => {
+    const iProps = { color: '#1675e0' };
+
     const [readonly, setReadonly] = useState(false);
-    const [cards, setCards] = useState([...Array(18).keys()].map((value, index) => ({
-        id: index,
+    const [cards, setCards] = useState([...Array(18).keys()].map(value => ({
+        id: uuid(),
         content: {
             title: `Card Title ${value}`,
             description: `
@@ -29,21 +32,22 @@ const App = () => {
     const handleEdit = (id, content) => setCards(cards.map(card => card.id === id ? { ...card, content } : card));
     const handleCheck = (id, checked) => setCards(cards.map(card => card.id === id ? { ...card, checked } : card));
     const handleDelete = () => setCards(cards.filter(({ checked }) => !checked));
+    const handleAdd = () => setCards([...cards, {
+        id: uuid(),
+        content: {
+            title: 'Edit me!',
+            description: 'Me too :)'
+        },
+        checked: Math.floor(Math.random() * 5) + 1
+    }]);
 
     return (
         <div className={styles.app}>
             <Header title="React App" />
             <Menu>
-                <Checkbox
-                    name="Readonly"
-                    checked={readonly}
-                    onChange={handleReadonly}
-                />
-                <button
-                    className={iButton}
-                    onClick={handleDelete}
-                    children={<MdDelete color="#1675e0" />}
-                />
+                <Checkbox name="Readonly" checked={readonly} onChange={handleReadonly} />
+                <button className={iButton} onClick={handleAdd} children={<MdAdd {...iProps} />} />
+                <button className={iButton} onClick={handleDelete} children={<MdDelete {...iProps} />} />
             </Menu>
             <Content>
                 {cards.length ? (
